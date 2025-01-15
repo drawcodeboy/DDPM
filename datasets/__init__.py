e97494920f01c176a3104d743dff14433d6e63a8
drawcodeboy <20rnjsekdns@naver.com>
2025-01-15 22:17:39 +0900

    [Add] initial files

---
+++ datasets/__init__.py
@@ -0,0 +1,6 @@
+from .mnist_dataset import MNIST_Dataset
+
+def load_dataset(dataset="MNIST",
+                 mode='train'):
+    if dataset == "MNIST":
+        return MNIST_Dataset(mode=mode)
---
+++ models/DDPM/UNet.py
@@ -0,0 +1,8 @@
+import torch
+from torch import nn
+
+class UNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self, x)
