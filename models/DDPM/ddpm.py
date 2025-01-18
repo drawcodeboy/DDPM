9b207d53b470b3c0a794714ff250ca8c81ee8feb
drawcodeboy <20rnjsekdns@naver.com>
2025-01-19 00:35:43 +0900

    [Add] settings Alpha, Beta & working on Algorithm 1

---
+++ models/DDPM/ddpm.py
@@ -0,0 +1,53 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class DDPM(nn.Module):
+    def __init__(self,
+                 time_steps: int = 1000,
+                 beta_schedule: str = 'linear'):
+        super().__init__()
+        
+        # 1) Alpha & Beta Settings
+        if beta_schedule == 'linear':
+            betas = self._beta_linear_schedule(time_steps)
+        else:
+            raise Exception(f"beta_schdule {beta_schedule} is not supported yet.")
+        
+        alphas = 1. - betas
+        alphas_cumprod = torch.cumprod(alphas, dim=0) # Cumulative Product
+        
+        self.register_buffer('betas', betas)
+        self.register_buffer('alphas_cumprod', alphas_cumprod)
+        
+        # 2) Coefficients Using Alpha & Beta
+        self.register_buffer('sqrt_alphas_cumprod', torch.sqrt(alphas_cumprod))
+        self.register_buffer('sqrt_one_minus_alphas_cumprod', torch.sqrt(1.-alphas_cumprod))
+        
+        # 3) Time Step Settings
+        self.time_steps = time_steps
+        
+    def _beta_linear_schedule(self, time_steps):
+        start, end = 0.0001, 0.02
+        
+        return torch.linspace(start, end, time_steps)
+    
+    def _algorithm_1(self, x_0):
+        # Sampling t from Uniform dist, without 0
+        # Because x_0 prediction needs discerete decoder.
+        bz = x_0.shape[0] # Batch Size
+        t = torch.randint(1, self.time_steps, (bz,), device=x.device)
+        
+        noise = torch.rand_like(x_0) # noise; eps
+        # START IMPLEMENTATION HERE!!
+    
+    def forward(self, x):
+        self._algorithm_1(x_0)
+
+        return t
+
+if __name__ == '__main__':
+    model = DDPM()
+    
+    time_steps = model(torch.randn(3, 3))
+    print(time_steps)
