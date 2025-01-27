952d70a162456629bdc0d81bd6a240813f1ff181
drawcodeboy <20rnjsekdns@naver.com>
2025-01-27 13:06:35 +0900

    [Add] U-Net args

--- models/__init__.py
+++ models/__init__.py
@@ -3,4 +3,13 @@ from .DDPM.ddpm import DDPM
 def load_model(**cfg):
     if cfg['name'] == 'DDPM':
         return DDPM(time_steps=cfg['time_steps'],
-                    beta_schedule=cfg['beta_schedule'])
+                    beta_schedule=cfg['beta_schedule'],
+                    input_dim=cfg['input_dim'],
+                    init_dim=cfg['init_dim'],
+                    dim_mults=cfg['dim_mults'],
+                    time_emb_theta=cfg['time_emb_theta'],
+                    time_emb_dim=cfg['time_emb_dim'],
+                    attn_emb_dim=cfg['attn_emb_dim'],
+                    attn_heads=cfg['attn_heads'],
+                    dropout_rate=cfg['dropout_rate'],
+                    device=cfg['device'])
