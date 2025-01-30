a7605ab10e33029ca146553aa521b922e0d5e42b
drawcodeboy <20rnjsekdns@naver.com>
2025-01-30 15:13:49 +0900

    [Complete] Algorithm 2 Success!!!

--- models/DDPM/ddpm.py
+++ models/DDPM/ddpm.py
@@ -3,6 +3,7 @@ from torch import nn
 import torch.nn.functional as F
 
 from einops import reduce
+
 from typing import Tuple
 from functools import partial
 
@@ -32,40 +33,20 @@ class DDPM(nn.Module):
         alphas = 1. - betas
         alphas_cumprod = torch.cumprod(alphas, dim=0) # Cumulative Product
         
-        self.register_buffer('alphas', alphas)
-        self.register_buffer('betas', betas)
-        self.register_buffer('alphas_cumprod', alphas_cumprod)
+        self.register_buffer('alphas', alphas.to(torch.float32))
+        self.register_buffer('betas', betas.to(torch.float32))
+        self.register_buffer('alphas_cumprod', alphas_cumprod.to(torch.float32))
         
         # 2.1) Coefficients Using Alpha & Beta
-        self.register_buffer('sqrt_alphas_cumprod', torch.sqrt(alphas_cumprod)) # Used in Algorithm 1
-        self.register_buffer('sqrt_one_minus_alphas_cumprod', torch.sqrt(1.-alphas_cumprod)) # Used in Algorithm 1, 2
-        self.register_buffer('sqrt_alphas', torch.sqrt(alphas)) # Used in Algorithm 2
-        self.register_buffer('one_minus_alphas', betas) # Used in Algorithm 2
+        self.register_buffer('sqrt_alphas_cumprod', torch.sqrt(alphas_cumprod).to(torch.float32)) # Used in Algorithm 1
+        self.register_buffer('sqrt_one_minus_alphas_cumprod', torch.sqrt(1.-alphas_cumprod).to(torch.float32)) # Used in Algorithm 1, 2
         
-        '''
-        # Ignore this
-        # used in self.sample, misunderstanding function from algorithm 2, Eq.4
-        # 2.2) Variance
+        # 2.2) P Variance (used in Algorithm 2)
         alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.)
-        self.register_buffer('variance', ((1.-alphas_cumprod_prev)/(1.-alphas_cumprod))*self.betas)
-        '''
-        
-        # 2.2) Used in get_x_0_from_model_noise_pred to get x_0
-        self.register_buffer('sqrt_recip_alphas_cumprod', torch.sqrt(1.-alphas_cumprod))
-        self.register_buffer('sqrt_recipm1_alphas_cumprod', torch.sqrt(1. / alphas_cumprod - 1))
-        
-        # 2.3) Posterior Value q(x_{t-1} | x_t, x_0)
-        
-        # 2.3.1) Posterior Variance
-        alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.)
-        posterior_var = betas * (1.-alphas_cumprod_prev) / (1.-alphas_cumprod)
-        self.register_buffer('posterior_var', posterior_var)
+        p_var = betas * (1.-alphas_cumprod_prev) / (1.-alphas_cumprod)
+        self.register_buffer('p_var', p_var.to(torch.float32))
         # In log calculation, 0 will go to -inf, and variance can get 0. In brief, we need clamp
-        self.register_buffer('posterior_log_var_clipped', torch.log(posterior_var.clamp(min=1e-20)))
-        
-        # 2.3.2) Posterior Mean Coefficients
-        self.register_buffer('posterior_mean_coef1', betas*torch.sqrt(alphas_cumprod_prev)/(1.-alphas_cumprod))
-        self.register_buffer('posterior_mean_coef2', (1. - alphas_cumprod_prev) * torch.sqrt(alphas) / (1. - alphas_cumprod))
+        self.register_buffer('p_log_var_clipped', torch.log(p_var.clamp(min=1e-20)).to(torch.float32))
         
         # 3) Time Step Settings
         self.time_steps = time_steps
@@ -74,7 +55,10 @@ class DDPM(nn.Module):
         self.model = UNet(input_dim=input_dim,
                           init_dim=init_dim,
                           dim_mults=dim_mults,
+                          time_emb_theta=time_emb_theta,
                           time_emb_dim=time_emb_dim,
+                          attn_emb_dim=attn_emb_dim,
+                          attn_heads=attn_heads,
                           dropout_rate=dropout_rate)
         
         # Extra
@@ -83,7 +67,7 @@ class DDPM(nn.Module):
     def _beta_linear_schedule(self, time_steps):
         start, end = 0.0001, 0.02
         
-        return torch.linspace(start, end, time_steps) # [start, ..., end], includes end
+        return torch.linspace(start, end, time_steps, dtype=torch.float64) # [start, ..., end], includes end
     
     def _normalize(self, x):
         # DDPM needs [-1, 1] scale input, it is related to All process performs N(0,I)
@@ -153,42 +137,24 @@ class DDPM(nn.Module):
         
         return loss.mean()
     
-    def get_x_0_from_model_noise_pred(self, x_t, t):
-        # t: shape = (b,) | dtype = torch.int64 | full of timesteps
-        # Predict Noise
+    def p_mean_variance(self, x_t, t):
+        # [4] get x_{t-1}
         noise_pred = self.model(x_t, t)
-        
-        # Get x_0 from noise_pred and x_t
-        x_0 = self._extract(self.sqrt_recip_alphas_cumprod, t, x_t.shape) * x_t
-        x_0 -= self._extract(self.sqrt_recipm1_alphas_cumprod, t, x_t.shape) * noise_pred
-        
-        return x_0
-    
-    def q_posterior(self, x_0, x_t, t):
-        # KEY POINT, Get posterior mean using x_0, x_t
-        posterior_mean = self._extract(self.posterior_mean_coef1, t, x_t.shape) * x_0
-        posterior_mean += self._extract(self.posterior_mean_coef2, t, x_t.shape) * x_t
+
+        p_mean = x_t - (self._extract(self.betas, t, x_t.shape) / self._extract(self.sqrt_one_minus_alphas_cumprod, t, x_t.shape)) * noise_pred
+        p_mean = p_mean / self._extract(self.alphas, t, x_t.shape)
         
         # Variance is fixed value
-        posterior_var = self._extract(self.posterior_var, t, x_t.shape)
-        posterior_log_var_clipped = self._extract(self.posterior_log_var_clipped, t, x_t.shape)
+        p_var = self._extract(self.p_var, t, x_t.shape)
+        p_log_var_clipped = self._extract(self.p_log_var_clipped, t, x_t.shape)
         
-        return posterior_mean, posterior_var, posterior_log_var_clipped
-    
-    def p_mean_variance(self, x_t, t):
-        # If you want to get P's mean, variance
-        # You should get x_0, and calculate it like Forward Process Q(x_{t-1}|x_{t}, x_0)
-        # And, x_0 can be acquired by x_t, t, noise which from model prediction
-        x_0 = self.get_x_0_from_model_noise_pred(x_t, t)
-        
-        p_mean, p_var, p_log_var = self.q_posterior(x_0=x_0, x_t=x_t, t=t)
-        return p_mean, p_var, p_log_var
+        return p_mean, p_var, p_log_var_clipped
     
     @torch.inference_mode()
-    def sample(self, x_t, t):
-        # Sampling from P(x_{t-1}|x_{t})
+    def sample(self, x_t, t, clip_scaled=True):
         # [3] z ~ N(0, I)
         z = torch.randn_like(x_t)
+
         # [4] get x_{t-1}
         t_ = torch.full((x_t.shape[0],), t, device=self.device, dtype=torch.int64)
         
@@ -196,26 +162,12 @@ class DDPM(nn.Module):
         p_mean, p_var, p_log_var = self.p_mean_variance(x_t=x_t, t=t_)
         x_t_minus_one = p_mean + (0.5 * p_log_var).exp() * z
         
-        return x_t_minus_one
-    '''
-    # Ignore this
-    # This function stems from a misunderstanding of Equation 4 in Algorithm 2. 
-    # To compute the mean from the noise, it is necessary to calculate x_0
-    # from the predicted noise and use it to derive the mean, 
-    # rather than deriving it directly in an intuitive manner as described in the paper.
-    @torch.inference_mode()
-    def sample(self, x_t, t):
-        # [3] z ~ N(0, I)
-        z = torch.randn_like(x_t)
-        
-        # [4] get x_{t-1}
-        t_ = torch.full((x_t.shape[0],), t, device=self.device, dtype=torch.int64)
-        noise_pred = self.model(x_t, t_)
-        x_t_minus_one = (1/self.sqrt_alphas[t]) * (x_t-(self.one_minus_alphas[t]/self.sqrt_one_minus_alphas_cumprod[t])*noise_pred)
-        x_t_minus_one += self.variance[t] * z
+        if clip_scaled:
+            # It guarantees sampling quality.
+            # Because, it trained data scale [-1, 1]
+            x_t_minus_one.clamp_(-1., 1.)
         
         return x_t_minus_one
-    '''
     
     @torch.inference_mode()
     def algorithm2(self, shape, get_all_timesteps=False, verbose=False):
@@ -257,6 +209,7 @@ class DDPM(nn.Module):
         
     def view(self, x_0):
         # view forward or reverse process (that depends on post-processing)
+        # Not used in training or sampling
         x_0 = self._normalize(x_0)
         noise = torch.randn_like(x_0)
         
